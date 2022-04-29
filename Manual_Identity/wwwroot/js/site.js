dd97cef939efd1cfea453997a8d31a16b76e377e
AmmarQamar <rajaammarqamar@gmail.com>
2022-04-29 12:54:17 +0500

    Join and Assign Roles to Users

--- Manual_Identity/wwwroot/js/site.js
+++ Manual_Identity/wwwroot/js/site.js
@@ -1,21 +1,3 @@
-﻿
-$("#success").function () {
-    $(".notify").toggleClass("active");
-    $("#notifyType").toggleClass("success");
-
-    setTimeout(function () {
-        $(".notify").removeClass("active");
-        $("#notifyType").removeClass("success");
-    }, 2000);
-};
-
-$("#failure").click(function () {
-    $(".notify").addClass("active");
-    $("#notifyType").addClass("failure");
-
-    setTimeout(function () {
-        $(".notify").removeClass("active");
-        $("#notifyType").removeClass("failure");
-    }, 2000);
+﻿$(document).ready(function () {
+    $('#table_id').DataTable();
 });
-
