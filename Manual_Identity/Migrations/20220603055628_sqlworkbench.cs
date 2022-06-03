437371fe56698d0e028ad53783954e930c8f8b6d
AmmarQamar <rajaammarqamar@gmail.com>
2022-06-03 11:05:35 +0500

    cart work proper

---
+++ Manual_Identity/Migrations/20220603055628_sqlworkbench.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Manual_Identity.Migrations
+{
+    public partial class sqlworkbench : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
